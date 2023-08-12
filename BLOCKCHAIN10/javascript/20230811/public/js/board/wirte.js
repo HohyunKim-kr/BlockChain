2be876f78e8d02719e0233b3dfd8bd6d42805dc4
Hohyun.Kim <hohyun1022@naver.com>
2023-08-12 22:25:54 +0900

    Create : 20230812

--- BLOCKCHAIN10/javascript/20230811/public/js/board/wirte.js
+++ BLOCKCHAIN10/javascript/20230811/public/js/board/wirte.js
@@ -1,27 +1,27 @@
-import BoardRow from "../lib/BoardRow.js"
-import Storage from "../lib/storage.js"
+import BoardRow from "../lib/BoardRow.js";
+import Storage from "../lib/storage.js";
 
-const storage = new Storage()
-const form = document.querySelector("#writeform")
+const storage = new Storage();
+const form = document.querySelector("#writeform");
 
 form.addEventListener("submit", (e) => {
-    e.preventDefault()
+  e.preventDefault();
 
-    const title = e.target.title.value
-    const writer = e.target.writer.value
-    const content = e.target.content.value
+  const title = e.target.title.value;
+  const writer = e.target.writer.value;
+  const content = e.target.content.value;
 
-    // 마지막 아이템 id + 1
-    const id = storage.incrementId()
-    const dataRow = {
-        id: id,
-        title: title,
-        writer: writer,
-        content: content,
-    }
-    const boardRow = new BoardRow(dataRow)
+  // 마지막 아이템 id + 1
+  const id = storage.incrementId();
+  const dataRow = {
+    id: id,
+    title: title,
+    writer: writer,
+    content: content,
+  };
+  const boardRow = new BoardRow(dataRow);
 
-    storage.set(boardRow)
+  storage.set(boardRow);
 
-    location.href = `/board/view.html?id=${id}`
-})
+  location.href = `/board/view.html?id=${id}`;
+});
